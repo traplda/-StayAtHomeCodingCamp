32cfd4116bed56920789c8544ca9851c44814b58
traplda <traplda@vscht.cz>
2020-04-23 17:09:34 +0200

    first js

---
+++ js_firstime/scripts.js
@@ -0,0 +1,12 @@
+let name = 'Dalibor'
+
+console.log(name + "'s apples")
+console.log("Hello world!")
+
+$('h1').text("We are having a 10 minutes break until 17:05")
+
+// This is a Javascript comment
+
+/*
+This is a multiline comment
+*/
